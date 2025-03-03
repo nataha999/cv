0e5d68e25e30201cb71ce946225285442dac0dd9
nataha <SNatawaN1@gmail.com>
2025-03-03 21:11:56 +0300

    add lw3

---
+++ app/page.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Мое резюме</h1>
+      <section>
+        <h2>Контакты</h2>
+        <p>Email: snatawan1@gmail.com</p>
+        <p>Phone Number: +79024310910</p>
+        <p>
+          GitHub:{" "}
+          <a href="https://github.com/nataha999">github.com/nataha999</a>
+        </p>
+      </section>
+      <section>
+        <h2>Образование</h2>
+        <p>Моя школа</p>
+        <p>
+          Поволжский государственный технологический университет, 4 курс, высшее
+          неоконченное
+        </p>
+      </section>
+    </div>
+  );
+}

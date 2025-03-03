d9110965728bb53b8eba11a18ebee50d125aef70
nataha <SNatawaN1@gmail.com>
2025-03-03 21:29:06 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const isProduction = process.env.NODE_ENV === "production";
 const repoName = "cv";
-
-module.exports = {
-  assetPrefix: isProduction ? `/${repoName}/` : "",
-  basePath: isProduction ? `/${repoName}` : "",
-  trailingSlash: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", 
+  assetPrefix: isProduction ? `/${repoName}/` : "", 
+  basePath: isProduction ? `/${repoName}` : "", 
+  trailingSlash: true, 
+  images: {
+    unoptimized: true, 
+  },
 };
+
+module.exports = nextConfig;
